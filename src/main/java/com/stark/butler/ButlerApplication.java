8aaf6d2e809b8f7e05922176eaa9c86ae996c1d0
DavydenkoAnton <davydenko83@gmail.com>
2022-06-28 10:13:29 +0300

    rename service folders

--- src/main/java/com/stark/butler/ButlerApplication.java
+++ src/main/java/com/stark/butler/ButlerApplication.java
@@ -3,7 +3,7 @@ package com.stark.butler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// git token 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
+// git token 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
 @SpringBootApplication
 public class ButlerApplication {
 
