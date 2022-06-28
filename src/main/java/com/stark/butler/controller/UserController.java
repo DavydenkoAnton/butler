12aa8a4cd87bb012981b303978887ffc13ee9a64
DavydenkoAnton <davydenko83@gmail.com>
2022-06-28 22:41:03 +0300

    add method |getUserById(Long id)| to UserController

--- src/main/java/com/stark/butler/controller/UserController.java
+++ src/main/java/com/stark/butler/controller/UserController.java
@@ -1,11 +1,25 @@
 package com.stark.butler.controller;
 
+import com.stark.butler.entity.User;
 import com.stark.butler.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
+    public User getUserById(Long id) {
+        User user = new User();
+        Optional<User> optionalUser = userService.findUserById(id);
+        if (optionalUser.isPresent()) {
+            user = optionalUser.get();
+        }
+        return user;
+    }
 
 }
